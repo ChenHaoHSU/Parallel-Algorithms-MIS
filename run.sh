0d83ad9e467c5934bfb2cc63b3f81b939d54adbd
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2023-11-28 12:59:44 -0600

    rm unused inputs, upd run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,22 @@
 make clean
 make
+
 for i in {1..8}
 do
   echo "========================"
   echo "= Input $i"
   echo "========================"
-  ./bin/mis input/input$i.txt
+  ./bin/mis input/input$i.txt Blelloch 32
 done
+
+#for i in {8..8}
+#do
+  #for t in 1 2 4 8 16 32 64
+  #do
+    #echo "========================"
+    #echo "= Threads $t"
+    #echo "========================"
+    #./bin/mis input/input$i.txt Luby $t
+  #done
+#done
+
